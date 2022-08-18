9c784b60bff234c370919b1e09ccc3736f8433a6
AFOEK <felix.montalfu@gmail.com>
2022-08-18 17:03:29 +0700

    Adding API for redeem

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
+from typing import Optional
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix='>')
+bot = commands.Bot(command_prefix='!')
+
 @bot.event
 async def on_ready():
-    print(
-        f'{bot.user.name} has connected into discord'
-    )
+    print(f'{bot.user.name} has connected to Discord!') #just for debugging
 
-async def on_message(message):
-    if message.author == bot.user:
-        return
+@bot.command(name='redeem', help="Usage: !redeem 'uid' 'redeem_code' 'server_region'. If server region is ommited it will asume asia server")
+async def redeem(ctx,uid,redeem_code,srv_reg: Optional[str] = None):
+    print(f'{bot.user.name} getting input from discord client')
+    redeem_code = redeem_code.upper()   #This will make redeem code become capitalized
+    res=f'https://sg-hk4e-api.hoyoverse.com/common/apicdkey/api/webExchangeCdkey?uid={uid}&region=os_{srv_reg}&lang=en&cdkey={redeem_code}&game_biz=hk4e_global'    #redeem link
 
-async def on_error(event, *args, **kwargs):
-    with open('err.log','a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
+    if(uid.isnumeric() and len(uid)!=9):    #check if "UID" is UID
+        res = f'Invalid UID !'  #Error message
+
+    if(srv_reg is None):
+        srv_reg = 'asia'
+        res = f'Since no server region get specified, it will assume asia region\nhttps://sg-hk4e-api.hoyoverse.com/common/apicdkey/api/webExchangeCdkey?uid={uid}&region=os_{srv_reg}&lang=en&cdkey={redeem_code}&game_biz=hk4e_global'
+    print (uid,redeem_code,srv_reg)
+    await ctx.send(res)
 
 bot.run(TOKEN)
